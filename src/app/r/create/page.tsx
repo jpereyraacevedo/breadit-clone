1ffca3d4880031e60a66ccfd6da0312dabb35d2b
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-01-02 18:29:06 -0300

    Create Community

---
+++ src/app/r/create/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
+const Page = () => {
+  const [input, setInput] = useState<string>("")
+
+  return (
+    <div className="container flex items-center h-full max-w-3xl mx-auto">
+      <div className="relative bg-white w-full h-fit p-4 rounder-lg space-y-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-semibold">Create a Community</h1>
+        </div>
+        <hr className="bg-zinc-500 h-px" />
+        <div className="">
+          <p className="text-lg font-medium">
+            Name
+          </p>
+          <p className="text-xs pb-2">
+            Community names including capitalization cannot be changed.
+          </p>
+          <div className="relative">
+            <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
+              r/
+            </p>
+            <Input value={input} onChange={(e) => setInput(e.target.value)} className="pl-6" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page
