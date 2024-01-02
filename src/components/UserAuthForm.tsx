ff05928bdef648a313b6c389bb9f27a74310e404
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-01-01 23:56:44 -0300

    auth continuation

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -20,7 +20,6 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
         setIsLoading(true)
 
         try {
-            throw new Error()
             await signIn("google")
         } catch (error) {
             toast({
