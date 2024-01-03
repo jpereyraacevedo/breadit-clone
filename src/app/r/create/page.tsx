ef3522dec1e2df5a20cd0e477f4202a3e5a1754d
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-01-03 14:16:22 -0300

    subreddit creation finished

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,10 +1,62 @@
 "use client"
 
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/Button"
+import { Input } from "@/components/ui/Input"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { useMutation } from "@tanstack/react-query"
+import axios, { AxiosError } from "axios"
+import { CreateSubredditPayload } from "@/lib/validators/subreddit"
+import { toast } from "@/hooks/use-toast"
+import { useCustomToast } from "@/hooks/use-custom-toast"
 
 const Page = () => {
-  const [input, setInput] = useState<string>("")
+  const [input, setInput] = useState<string>("");
+  const router = useRouter();
+  const { loginToast } = useCustomToast()
+
+  const { mutate: createComunnity, isLoading } = useMutation({
+    mutationFn: async () => {
+
+      const payload: CreateSubredditPayload = {
+        name: input,
+      }
+      const { data } = await axios.post("/api/subreddit", payload)
+      return data as string
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Subreddit already exist",
+            description: "Please choose a different subreddit name.",
+            variant: "destructive",
+          })
+        }
+
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invalid subreddit name.",
+            description: "Please choose a name bewtween 3 and 21 characters.",
+            variant: "destructive",
+          })
+        }
+
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
+
+      toast({
+        title: "There was an error",
+        description: "Could not create subreddit.",
+        variant: "desctructive"
+      })
+    },
+    onSuccess: (data) => {
+      router.push(`/r/${data}`)
+    }
+  })
 
   return (
     <div className="container flex items-center h-full max-w-3xl mx-auto">
@@ -24,8 +76,20 @@ const Page = () => {
             <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
               r/
             </p>
-            <Input value={input} onChange={(e) => setInput(e.target.value)} className="pl-6" />
+            <Input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="pl-6" />
           </div>
+        </div>
+        <div className="flex justify-end gap-4">
+          <Button variant="subtle" onClick={() => router.back()}>Cancel</Button>
+          <Button
+            isLoading={isLoading}
+            disabled={input.length === 0}
+            onClick={() => createComunnity()}>
+            Create Community
+          </Button>
         </div>
       </div>
     </div>
