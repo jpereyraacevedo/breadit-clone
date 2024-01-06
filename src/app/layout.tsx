4c306a882f7b7788978e7db9565108158dfcb632
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-01-05 23:36:44 -0300

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google"
 
 export const metadata = {
   title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript..',
+  description: 'A Reddit clone built with Next.js and TypeScript...',
 }
 
 const inter = Inter({ subsets: ["latin"] })
