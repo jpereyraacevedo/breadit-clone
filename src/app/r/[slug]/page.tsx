a40a63cf1318ce4e60b2ee0dc8e7c6d0d4392a4c
Jonatan Pereyra <jpereyraacevedo@gmail.com>
2024-01-09 20:06:18 -0300

    -

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -45,7 +45,7 @@ const page = async ({ params }: PageProps) => {
       </h1>
       <MiniCreatePost session={session} />
       <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
-      {/* TODO: Show posts in used feed */}
+      {/* TODO: Show posts in. used feed */}
     </>
   )
 }
